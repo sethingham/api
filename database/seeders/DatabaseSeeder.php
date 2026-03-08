c398df94b280589811413d1b4bbc1267e6052a8a
Seth Bossingham <seth.bossingham@gmail.com>
2026-03-08 11:38:20 -0400

    use seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProfileSeeder::class,
+            TechnologySeeder::class,
+            ProjectSeeder::class,
         ]);
     }
 }
